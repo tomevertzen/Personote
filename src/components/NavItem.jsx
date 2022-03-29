d3c00eaf6d94dcfbcd076482980cae162b95144f
Tom Evertzen <tom_evertzen@hotmail.com>
2022-03-29 17:16:44 +0200

    Fixed the first part of the styling of NavItem

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -3,10 +3,16 @@ import { NavLink } from "react-router-dom";
 import "boxicons";
 
 const NavItem = ({ title, iconName, path, iconType = "" }) => {
+  const activeClass = "text-black";
   return (
     <li>
       <NavLink
-        className="flex items-center gap-1 mb-1 hover:text-black "
+        className={({ isActive }) => {
+          return isActive
+            ? activeClass + " flex items-center gap-3 mb-1"
+            : "flex items-center gap-3 mb-1";
+        }}
+        // className="flex items-center gap-1 mb-1 hover:text-black "
         to={path}
       >
         <box-icon name={iconName} type={iconType} color="#9CA3AF"></box-icon>
